ea156eb562a2264f213e750e9810e35d54e09e12
Carol-88 <gralleta@gmail.com>
2024-05-04 20:54:26 +0200

    🚧

---
+++ src/components/Project/ProjectCard.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+
+export const ProjectCard = ({ banner, name, subtitle }) => (
+    
+ <article className="card shadow-lg hover:shadow-xl transition-shadow duration-300 w-full max-w-80">
+    <img
+      src={banner}
+      alt={name}
+      className="w-full h-64 object-cover shadow-md"
+    />
+    <div className="flex flex-col justify-center items-start gap-4 p-4">
+      <h2 className="text-xl font-semibold">{name}</h2>
+      <p className="text-gray-600 text-start">{subtitle}</p>
+      <a href="{`/proyecto/${id}`}" className="text-[#7f1d1d] font-semibold hover:font-bold">
+        Ver más
+      </a>
+    </div>
+ </article>
+);
+
+ProjectCard.propTypes = {
+ banner: PropTypes.string.isRequired,
+ name: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+ id:PropTypes.number.isRequired
+};
