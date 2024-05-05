f68b9973e81c27aaa658a782aa375913190af584
Carol-88 <gralleta@gmail.com>
2024-05-05 11:20:50 +0200

    🚧

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 
-export const ProjectCard = ({ banner, name, subtitle }) => (
+export const ProjectCard = ({ banner, name, subtitle, web, projectLink }) => (
     
  <article className="card shadow-lg hover:shadow-xl transition-shadow duration-300 w-full max-w-80">
     <img
@@ -10,9 +11,9 @@ export const ProjectCard = ({ banner, name, subtitle }) => (
       className="w-full h-64 object-cover shadow-md"
     />
     <div className="flex flex-col justify-center items-start gap-4 p-4">
-      <h2 className="text-xl font-semibold">{name}</h2>
+      <Link to={web} target="_blank"><h2 className="text-xl font-semibold hover:font-bold">{name}</h2></Link>
       <p className="text-gray-600 text-start">{subtitle}</p>
-      <a href="" className="text-[#7f1d1d] font-semibold hover:font-bold">
+      <a href={projectLink} className="text-[#7f1d1d] font-semibold hover:font-bold">
         Ver más
       </a>
     </div>
@@ -23,5 +24,7 @@ ProjectCard.propTypes = {
  banner: PropTypes.string.isRequired,
  name: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
- id:PropTypes.number.isRequired
+  id: PropTypes.number.isRequired,
+  web: PropTypes.string.isRequired,
+ projectLink: PropTypes.string.isRequired
 };
