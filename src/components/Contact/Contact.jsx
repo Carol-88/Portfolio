bf296a7a351053e5bce7d40bd96f16476849355b
Carol-88 <gralleta@gmail.com>
2024-05-18 17:08:55 +0200

    💄

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -31,7 +31,7 @@ export const Contact = () => {
               id="from_name"
               placeholder=" Nombre completo "
               required
-              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full rounded-md border  bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:shadow-md"
             />
           </div>
           <div className="mb-5">
@@ -42,7 +42,7 @@ export const Contact = () => {
               placeholder="tu-email@domain.com"
               required
               autoComplete="off"
-              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full rounded-md border  bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:shadow-md"
             />
           </div>
           <div className="mb-5">
@@ -52,7 +52,7 @@ export const Contact = () => {
               id="subject"
               placeholder=" Asunto "
               required
-              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full rounded-md border  bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:shadow-md"
             />
           </div>
           <div className="mb-5">
@@ -62,7 +62,7 @@ export const Contact = () => {
               id="message"
               placeholder=" Escribe tu mensaje "
               required
-              className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full resize-none rounded-md border bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:shadow-md"
             ></textarea>
           </div>
           <div>
