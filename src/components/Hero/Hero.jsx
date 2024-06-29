ad909fbc2777bd71ac80d9c5748d4c1bf5fa33bc
Carol-88 <gralleta@gmail.com>
2024-06-29 20:19:39 +0200

    work in icons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,34 +1,29 @@
 export const Hero = () => {
   return (
     <section id="hero" className=" bg-red-50 p-8 rounded-lg">
-      <h1 className="text-2xl md:text-5xl mb-8 text-red-800 font-bold hover:animate-bounce">
-        Desarrolladora Web Frontend
+      <h1 className="text-xl md:text-3xl mb-8 text-red-800 font-bold hover:animate-bounce">
+        ¡Amante del código y creadora de experiencias web!
       </h1>
       <article>
         <p className="text-start mb-4 ">
-          <img
-            src="/img/me.png"
-            alt="Mi retrato"
-            className="hidden float-left w-48 rounded-3xl mx-4 grayscale hover:grayscale-0 md:block"
-          />
-          Me considero afortunada de dedicarme a lo que amo:
-          <h3 className="font-bold">
-            crear aplicaciones web que resalten por su diseño, funcionalidad y
-            accesibilidad.
-          </h3>
+          Me considero afortunada de dedicarme a lo que amo: Soy una apasionada
+          del desarrollo web que se enorgullece de crear aplicaciones que
+          destacan por su diseño, funcionalidad y accesibilidad.
         </p>
         <p className="text-start mb-4 ">
-          Aunque soy consciente de que tengo mucho margen para crecer y
-          aprender, me enorgullece ofrecer soluciones digitales que marcan la
-          diferencia.
+          Soy consciente de que tengo un camino infinito por recorrer para
+          seguir aprendiendo, pero me motiva la emoción del aprendizaje
+          constante.
+        </p>
+        <p className="text-start mb-4">
+          Busco constantemente nuevas oportunidades para aplicar mis habilidades
+          y expandir mis horizontes. Mi objetivo es simple: aportar mi granito
+          de arena al éxito de cada proyecto.
         </p>
         <p className="text-start">
-          Estoy abierta a colaborar en proyectos que me permitan aplicar mis
-          habilidades y, al mismo tiempo, expandir mis horizontes. Mi objetivo
-          es contribuir con mi granito de arena al éxito de cada proyecto,
-          trabajando en equipo para desarrollar experiencias web que no solo
-          sean visualmente atractivas sino también eficaces y significativas
-          para los usuarios finales.
+          Me encanta trabajar en equipo para desarrollar experiencias web que no
+          solo sean visualmente atractivas, sino también eficaces y
+          significativas para los usuarios finales.
         </p>
       </article>
     </section>
--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -2,7 +2,7 @@ import { RRSS } from "../commons/RRSS";
 
 export const ProfileCard = () => {
   return (
-    <div className=" flex justify-center p-5 sm:hidden md:hidden lg:hidden xl:hidden bg-gradient-to-r from-orange-200 via-rose-200 to-purple-200">
+    <div className=" flex justify-center p-5 sm:hidden md:hidden lg:hidden xl:hidden bg-gradient-to-r from-orange-200 via-rose-200 to-orange-200">
       <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96 ">
         <div className="relative pt-4 mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl saturate-50 hover:saturate-100">
           <a href="/">
@@ -19,7 +19,9 @@ export const ProfileCard = () => {
           </h4>
           <p>Diseño y desarrollo Web</p>
         </div>
-        <RRSS />
+        <div>
+          <RRSS />
+        </div>
       </div>
     </div>
   );
