c27b875a61a4337a0ebe2467ccf1d077e662040a
Carol-88 <gralleta@gmail.com>
2025-03-13 19:02:32 +0100

    correct links and scroll in project details

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,25 +1,31 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export const ProjectCard = ({ banner, name, web }) => (
-  <div className="min-w-[200px] sm:min-w-[250px] md:min-w-[300px] flex-shrink-0">
-    <article className="shadow-lg hover:shadow-xl bg-white rounded-xl overflow-hidden m-2">
-      <img src={banner} alt={name} className="w-full h-40 object-cover" />
-      <div className="p-2 text-center">
-        <Link
-          to={web}
-          target="_blank"
-          className="text-lg font-semibold hover:underline"
-        >
-          {name}
-        </Link>
-      </div>
-    </article>
-  </div>
-);
+export const ProjectCard = ({ banner, name, id }) => {
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <div className="min-w-[200px] sm:min-w-[250px] md:min-w-[300px] flex-shrink-0">
+      <article className="shadow-lg hover:shadow-xl bg-white rounded-xl overflow-hidden m-2">
+        <img src={banner} alt={name} className="w-full h-40 object-cover" />
+        <div className="p-2 text-center">
+          <Link
+            to={`/projects/${id}`}
+            onClick={handleClick}
+            className="text-lg font-semibold hover:underline"
+          >
+            {name}
+          </Link>
+        </div>
+      </article>
+    </div>
+  );
+};
 
 ProjectCard.propTypes = {
   banner: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  web: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,11 +1,11 @@
 import { ProjectDetails } from "../../components/Projects/ProjectDetails";
-import { SlideCards } from "../../components/Projects/SlideCards";
+import { ProjectSlider } from "../../components/Projects/ProjectSlider";
 
 export const ProjectPage = (id) => {
   return (
     <div className="container max-w-4xl mx-auto px-6 py-4 mt-4 bg-red-50 shadow-xl rounded-lg overflow-hidden text-pretty">
       <ProjectDetails id={id} />
-      <SlideCards />
+      <ProjectSlider />
     </div>
   );
 };
